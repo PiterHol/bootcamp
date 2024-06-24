7b1ee32df9fc7ae65e75ec01a591609c16f8c0b8
PiterHol <piotrek@satserwis.com.pl>
2024-06-24 19:09:04 +0200

    Added counter

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -2,5 +2,23 @@
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
     Siema!!!
+    <br>
+    {{ licznik }}
+    <button @click="podbij">podbij</button>
   </main>
 </template>
+
+<script>
+export default{
+  data(){
+    return{
+      licznik: 0
+    }
+  },
+  methods:{
+    podbij(){
+      this.licznik++
+    }
+  }
+}
+</script>
